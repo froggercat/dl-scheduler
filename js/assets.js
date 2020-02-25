9818f246f9f86fd11aa63ed39e934256305da544
Sondra Miller <sondra@csilongwood.com>
2020-02-25 11:14:24 -0500

    lead everything with a / for ... root?

--- js/assets.js
+++ js/assets.js
@@ -4,8 +4,8 @@ export default ([
             {
                 "element": "wind",
                 "images": {
-                    "2x": "../assets/2x_catoblepas_anemos.png",
-                    "path": "../assets/catoblepas_anemos.png"
+                    "2x": "/assets/2x_catoblepas_anemos.png",
+                    "path": "/assets/catoblepas_anemos.png"
                 },
                 "title": "Catoblepas Anemos"
             }
@@ -17,16 +17,16 @@ export default ([
             {
                 "element": "shadow",
                 "images": {
-                    "2x": "../assets/2x_ebon_chimera.png",
-                    "path": "../assets/ebon_chimera.png"
+                    "2x": "/assets/2x_ebon_chimera.png",
+                    "path": "/assets/ebon_chimera.png"
                 },
                 "title": "Ebon Chimera"
             },
             {
                 "element": "fire",
                 "images": {
-                    "2x": "../assets/2x_volcanic_chimera.png",
-                    "path": "../assets/volcanic_chimera.png"
+                    "2x": "/assets/2x_volcanic_chimera.png",
+                    "path": "/assets/volcanic_chimera.png"
                 },
                 "title": "Volcanic Chimera"
             }
@@ -38,8 +38,8 @@ export default ([
             {
                 "element": "wind",
                 "images": {
-                    "2x": "../assets/2x_eolian_phantom.png",
-                    "path": "../assets/eolian_phantom.png"
+                    "2x": "/assets/2x_eolian_phantom.png",
+                    "path": "/assets/eolian_phantom.png"
                 },
                 "title": "Eolian Phantom"
             }
@@ -51,15 +51,15 @@ export default ([
             {
                 "element": "water",
                 "images": {
-                    "2x": "../assets/2x_frost_hermit.png",
-                    "path": "../assets/frost_hermit.png"
+                    "2x": "/assets/2x_frost_hermit.png",
+                    "path": "/assets/frost_hermit.png"
                 },
                 "title": "Frost Hermit"
             },
             {
                 "element": "shadow",
                 "images": {
-                    "2x": "../assets/2x_twilight_hermit.png"
+                    "2x": "/assets/2x_twilight_hermit.png"
                 },
                 "title": "Twilight Hermit"
             }
@@ -71,24 +71,24 @@ export default ([
             {
                 "element": "water",
                 "images": {
-                    "2x": "../assets/2x_greedy_manticore.png",
-                    "path": "../assets/greedy_manticore.png"
+                    "2x": "/assets/2x_greedy_manticore.png",
+                    "path": "/assets/greedy_manticore.png"
                 },
                 "title": "Greedy Manticore"
             },
             {
                 "element": "light",
                 "images": {
-                    "2x": "../assets/2x_proud_manticore.png",
-                    "path": "../assets/proud_manticore.png"
+                    "2x": "/assets/2x_proud_manticore.png",
+                    "path": "/assets/proud_manticore.png"
                 },
                 "title": "Proud Manticore"
             },
             {
                 "element": "shadow",
                 "images": {
-                    "2x": "../assets/2x_raging_manticore.png",
-                    "path": "../assets/raging_manticore.png"
+                    "2x": "/assets/2x_raging_manticore.png",
+                    "path": "/assets/raging_manticore.png"
                 },
                 "title": "Raging Manticore"
             }
@@ -100,22 +100,22 @@ export default ([
             {
                 "element": "light",
                 "images": {
-                    "2x": "../assets/2x_lambent_ghost.png",
-                    "path": "../assets/lambent_ghost.png"
+                    "2x": "/assets/2x_lambent_ghost.png",
+                    "path": "/assets/lambent_ghost.png"
                 },
                 "title": "Lambent Ghost"
             },
             {
                 "element": "fire",
                 "images": {
-                    "path": "../assets/blazing_ghost.png"
+                    "path": "/assets/blazing_ghost.png"
                 },
                 "title": "Blazing Ghost"
             },
             {
                 "element": "shadow",
                 "images": {
-                    "path": "../assets/violet_ghost.png"
+                    "path": "/assets/violet_ghost.png"
                 },
                 "title": "Violet Ghost"
             }
@@ -127,23 +127,23 @@ export default ([
             {
                 "element": "fire",
                 "images": {
-                    "2x": "../assets/2x_scalding_shroom.png",
-                    "path": "../assets/scalding_shroom.png"
+                    "2x": "/assets/2x_scalding_shroom.png",
+                    "path": "/assets/scalding_shroom.png"
                 },
                 "title": "Scalding Shroom"
             },
             {
                 "element": "light",
                 "images": {
-                    "2x": "../assets/2x_wandering_shroom.png",
-                    "path": "../assets/wandering_shroom.png"
+                    "2x": "/assets/2x_wandering_shroom.png",
+                    "path": "/assets/wandering_shroom.png"
                 },
                 "title": "Wandering Shroom"
             },
             {
                 "element": "wind",
                 "images": {
-                    "path": "../assets/gust_shroom.png"
+                    "path": "/assets/gust_shroom.png"
                 },
                 "title": "Gust Shroom"
             }
@@ -155,40 +155,40 @@ export default ([
             {
                 "element": "fire",
                 "images": {
-                    "2x": "../assets/2x_void_agni.png",
-                    "path": "../assets/void_agni.png"
+                    "2x": "/assets/2x_void_agni.png",
+                    "path": "/assets/void_agni.png"
                 },
                 "title": "Void Agni"
             },
             {
                 "element": "light",
                 "images": {
-                    "2x": "../assets/2x_void_jeanne.png",
-                    "path": "../assets/void_jeanne.png"
+                    "2x": "/assets/2x_void_jeanne.png",
+                    "path": "/assets/void_jeanne.png"
                 },
                 "title": "Void Jeanne"
             },
             {
                 "element": "shadow",
                 "images": {
-                    "2x": "../assets/2x_void_nidhogg.png",
-                    "path": "../assets/void_nidhogg.png"
+                    "2x": "/assets/2x_void_nidhogg.png",
+                    "path": "/assets/void_nidhogg.png"
                 },
                 "title": "Void Nidhogg"
             },
             {
                 "element": "water",
                 "images": {
-                    "2x": "../assets/2x_void_poseidon.png",
-                    "path": "../assets/void_poseidon.png"
+                    "2x": "/assets/2x_void_poseidon.png",
+                    "path": "/assets/void_poseidon.png"
                 },
                 "title": "Void Poseidon"
             },
             {
                 "element": "wind",
                 "images": {
-                    "2x": "../assets/2x_void_zephyr.png",
-                    "path": "../assets/void_zephyr.png"
+                    "2x": "/assets/2x_void_zephyr.png",
+                    "path": "/assets/void_zephyr.png"
                 },
                 "title": "Void Zephyr"
             }
@@ -200,21 +200,21 @@ export default ([
             {
                 "element": "light",
                 "images": {
-                    "path": "../assets/amber_golem.png"
+                    "path": "/assets/amber_golem.png"
                 },
                 "title": "Amber Golem"
             },
             {
                 "element": "shadow",
                 "images": {
-                    "path": "../assets/obsidian_golem.png"
+                    "path": "/assets/obsidian_golem.png"
                 },
                 "title": "Obsidian Golem"
             },
             {
                 "element": "fire",
                 "images": {
-                    "path": "../assets/steel_golem.png"
+                    "path": "/assets/steel_golem.png"
                 },
                 "title": "Steel Golem"
             }
